1a17ff3f8d5551437e5552d715b6c69e19e74394
vasugit1 <35460130+vasugit1@users.noreply.github.com>
2025-11-22 19:26:27 -0600

    Initial Commit

---
+++ liveOnAppStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  liveOnAppStore
+//
+//  Created by Sri Tummala on 11/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ liveOnAppStore/liveOnAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  liveOnAppStoreApp.swift
+//  liveOnAppStore
+//
+//  Created by Sri Tummala on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct liveOnAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
