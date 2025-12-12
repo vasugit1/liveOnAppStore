a199dddc1813fee17d9bf978b5b2fad0fcaf2382
vasugit1 <35460130+vasugit1@users.noreply.github.com>
2025-12-12 08:10:44 -0600

    initial commit

--- liveOnAppStore/ContentView.swift
+++ liveOnAppStore/ContentView.swift
@@ -1,24 +1,14 @@
-//
-//  ContentView.swift
-//  liveOnAppStore
-//
-//  Created by Sri Tummala on 11/22/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("I am live on app store")
+            .font(.title)
+            .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
